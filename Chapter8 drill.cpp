4c1101ac0631ec91a254b8f22a6afbbd3b9131f5
shingayichitaka <65043825+shingayichitaka@users.noreply.github.com>
2021-04-08 21:25:16 +0200

    Create Chapter8 drill.cpp

---
+++ Chapter8 drill.cpp
@@ -0,0 +1,40 @@
+// Chapter 08, drill
+
+#include "std_lib_facilities.h"
+namespace X {
+    int var;
+    void print()
+    {
+        cout << var << endl;
+    }
+}
+namespace Y {
+    int var;
+    void print()
+    {
+        cout << var << endl;
+    }
+}
+namespace Z {
+    int var;
+    void print()
+    {
+        cout << var << endl;
+    }
+}
+int main()
+{
+    X::var = 7;
+    X::print();     
+    using namespace Y;
+    var = 9;
+    print(); 
+    {
+        using Z::var;
+        using Z::print;
+        var = 11;
+        print();
+    }
+    print();
+    X::print(); 
+}
